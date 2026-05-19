ff842c01607d66aa139134a54c413f2aadb7da8e
rajrai <rajrai913197@gmail.com>
2026-05-19 23:08:58 +0530

    new nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,11 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import Navbaar from "./component/nav"
 
-import "./style.css"
-let App = ()=>{
-  return (
+let App =()=>{
+  return(
     <>
-     <Container>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container id='main-nav'>
-          <Navbar.Brand id='logo' href="#">NexCart</Navbar.Brand>
-          <div id='links'>
-           <a href="">Home</a>
-           <a href="">Shop</a>
-           <a href="">About</a>
-          </div>
-          <div id='search-nav'>
-           <a href="">Search</a>
-           <a href="">Cart icon</a>
-           <button id='btn'>Login</button>
-          </div>
-        </Container>
-      </Navbar>
-    </Container>
-     <Carousel slide={false}>
-      <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <Navbaar />
     </>
+
   )
 }
 export default App
---
+++ src/component/nav.jsx
@@ -0,0 +1,17 @@
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+let Navbaar =()=>{
+    return(
+        <>
+     <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+      </Container>
+     </Navbar>
+        </>
+    )
+}
+export default Navbaar
