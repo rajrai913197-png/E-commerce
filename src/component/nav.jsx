d13af82df4a2e197c6ece4c2f1220308a9281acd
rajrai <rajrai913197@gmail.com>
2026-05-20 23:52:04 +0530

    new nav added

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -1,16 +1,40 @@
-
-import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 let Navbaar =()=>{
     return(
         <>
-     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#">Navbar</Navbar.Brand>
-      </Container>
-     </Navbar>
+         <Navbar className="bg-body-tertiary justify-content-between">
+      <Form inline>
+        <InputGroup>
+          <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+          <Form.Control
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+      </Form>
+      <Form inline>
+        <Row>
+          <Col xs="auto">
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className=" mr-sm-2"
+            />
+          </Col>
+          <Col xs="auto">
+            <Button type="submit">Submit</Button>
+          </Col>
+        </Row>
+      </Form>
+    </Navbar>
         </>
     )
 }
