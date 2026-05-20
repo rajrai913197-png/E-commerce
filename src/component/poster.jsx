2987d2adc9ce0754d66a101b38db75f46b826b0f
rajrai <rajrai913197@gmail.com>
2026-05-20 23:38:26 +0530

    new things add

--- src/component/poster.jsx
+++ src/component/poster.jsx
@@ -8,21 +8,21 @@ let Poster =()=>{
         <>
    <Carousel>
       <Carousel.Item>
-        <img src={f1} />
+        <img src={f1} style={{height:"450px", width:"100%"}}  />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src={f2} alt="" style={{height:"400px", width:"100%"}} />
+        <img src={f2} alt="" style={{height:"450px", width:"100%"}} />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src={f3} alt="" />
+        <img src={f3} alt="" style={{height:"450px", width:"100%"}}  />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
