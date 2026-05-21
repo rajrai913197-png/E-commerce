ee1a78c0029b166762ef5335db1434440f0619d1
rajrai <rajrai913197@gmail.com>
2026-05-21 23:49:01 +0530

    new

--- src/component/poster.jsx
+++ src/component/poster.jsx
@@ -1,28 +1,28 @@
 import Carousel from 'react-bootstrap/Carousel';
-import f1 from './imges/h1.jpg'
-import f2 from './imges/h2.jpg'
-import f3 from './imges/h3.jpg'
+import img1 from './images/shadi1.jpg'
+import img2 from './images/shadi2.jpg'
+import img3 from './images/shadi3.jpg'
 
 let Poster =()=>{
-    return(
-        <>
-   <Carousel>
+  return(
+  <>
+   <Carousel fade>
       <Carousel.Item>
-       
+        <img src={img1} alt="" style={{height:"500px",width:"100%"}} />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-
+       <img src={img2} alt="" style={{height:"500px",width:"100%"}} />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-       
+        <img src={img3} alt="" style={{height:"500px",width:"100%"}} />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
@@ -31,7 +31,8 @@ let Poster =()=>{
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-        </>
-    )
+  
+  </>
+  )
 }
-export default Poster
+export default Poster;
